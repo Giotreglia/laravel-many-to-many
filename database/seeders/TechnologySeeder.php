07784270d302279f0f3d0a9492a90621ce302681
Giotreglia <giovanni.treglia@gmail.com>
2023-05-25 15:12:27 +0200

    classe Technology creata con migration e seed fatte

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['Javascript', 'Html', 'Php', 'Laravel', 'Vue.js', 'Css', 'Bootstrap'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, '-');
+            $newTechnology->save();
+        }
     }
 }
