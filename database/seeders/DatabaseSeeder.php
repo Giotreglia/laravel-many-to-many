893b723da0cf62b305f6dee1265989dd90059dd0
Giotreglia <giovanni.treglia@gmail.com>
2023-05-25 15:24:45 +0200

    tabella pivot creata

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(ProjectSeeder::class);
+        $this->call(TechnologySeeder::class);
+        $this->call(TypeSeeder::class);
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
